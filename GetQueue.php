aa5ed67d7b35640554eb32343e90aeadd49f1562
Kristoffer <kra1992@hotmail.com>
2016-09-13 13:25:22 +0200

    Only show queue to admins

--- GetQueue.php
+++ GetQueue.php
@@ -5,8 +5,11 @@
  * Date: 12.09.2016
  * Time: 17.28
  */
+require_once 'Password.php';
 
-if(file_exists("Queue.txt")){
+$isAdmin = isset($_COOKIE['pw']) && $_COOKIE['pw'] = $PASSWORD;
+
+if(file_exists("Queue.txt") && $isAdmin){
     $file_content = file_get_contents("Queue.txt");
     $queue = unserialize($file_content);
     echo json_encode(array_values($queue));
